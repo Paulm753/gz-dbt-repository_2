f0729f2b01328ba047ed80ae5058f2e626024b6c
moutet.paul@gmail.com <moutet.paul@gmail.com>
2025-05-13 14:19:57 +0000

    challenge 3: question 2. 1,2,3

---
+++ models/staging/raw/stg_raw__sales.sql
@@ -0,0 +1,18 @@
+with 
+
+source as (
+    select * from {{ source('raw', 'sales') }}
+),
+
+renamed as (
+    select
+        date_date,
+        orders_id,
+        pdt_id as products_id,
+        quantity,
+        revenue
+    from source
+)
+
+select * from renamed
+
