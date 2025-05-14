2402df653fffa5bbacee46c1a2c075489df80a95
moutet.paul@gmail.com <moutet.paul@gmail.com>
2025-05-14 12:16:02 +0000

    test2

--- models/staging/raw/stg_raw__ship.sql
+++ models/staging/raw/stg_raw__ship.sql
@@ -1,22 +1,23 @@
-with 
+  -- stg_raw__ship.sql
 
-source as (
+  with
 
-    select * from {{ source('raw', 'ship') }}
+  source as (
 
-),
+      select * from {{ source('raw', 'ship') }}
 
-renamed as (
+  ),
 
-    select
-        orders_id,
-        shipping_fee,
-        shipping_fee_1,
-        logcost,
-        ship_cost
+  renamed as (
 
-    from source
+      select
+          orders_id,
+          shipping_fee,
+          logcost,
+          CAST(ship_cost AS INT64) AS ship_cost
 
-)
+      from source
 
-select * from renamed
+  )
+
+  select * from renamed
