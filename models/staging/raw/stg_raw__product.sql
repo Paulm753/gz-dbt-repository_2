d6650e2fd77dd7aa02e32b93c59290b08927dc30
moutet.paul@gmail.com <moutet.paul@gmail.com>
2025-05-14 12:12:34 +0000

    ok

--- models/staging/raw/stg_raw__product.sql
+++ models/staging/raw/stg_raw__product.sql
@@ -1,14 +1,19 @@
 with 
 
 source as (
+
     select * from {{ source('raw', 'product') }}
+
 ),
 
 renamed as (
+
     select
         products_id,
-        cast(purchSE_PRICE as float64) as purchase_price
+        purchse_price
+
     from source
+
 )
 
 select * from renamed
